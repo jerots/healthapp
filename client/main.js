998c467db4618cb23264b884274e2577c1131a32
edwardh1 <edwardharsono91@gmail.com>
2016-05-05 00:50:50 +0900

    Added form submission feature. However, the front end is not displaying properly whenever the form submission is activated.

--- client/main.js
+++ client/main.js
@@ -46,6 +46,58 @@ FlowRouter.route('/info/', {
 
 AppointmentList = new Mongo.Collection('appointment');
 
+Template.appointment.helpers({
+  newAppointment: function() {
+    return AppointmentList.findOne(Session.get('selectedApptId'));
+  }
+});
+
+
+Template.appointment.events = {
+  'click input[type=submit]': function(e) {
+    e.preventDefault();
+
+    var selectedApptId = Session.get('selectedApptId');
+    var newAppointment = AppointmentList.findOne(selectedPostId);
+
+    
+    var properties = {
+        Date: $('#date').val(),
+        AppointmentType: $('#apptType').val(),
+        PartiesInvolved: $('#partiesInvolved').val(),
+        Place: $('#place').val(),
+    };
+    AppointmentList.update(selectedApptId, {$set: properties});
+    },
+  'click .delete-link': function(e) {
+    // do something when the users clicks .delete-link
+  }
+};
+
+/*emplate.appointment.events={
+  'click input[type=submit]': function (event){
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    var date = target.date.value;
+    var apptType = target.apptType.value;
+    var partiesInvolved = target.partiesInvolved.value;
+    var place = target.place.value;
+ 
+    // Insert a task into the collection
+    AppointmentList.insert({
+      Date: date, AppointmentType: apptType, PartiesInvolved: partiesInvolved, Place: place
+    });
+ 
+    // Clear form
+    target.date.value = '';
+    target.apptType.value = '';
+    target.partiesInvolved.value = '';
+    target.place.value = '';
+  },
+};*/
 
 Template.Appointments.lists = function() {
     return AppointmentList.find();
